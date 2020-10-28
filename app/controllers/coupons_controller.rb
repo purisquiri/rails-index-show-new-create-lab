caa8303d4cf7da0e1733e9dce9b55661c0d9c41a
purisquiri <mau.dorigo@outlook.es>
2020-10-28 14:59:44 -0400

    getting some good stuff finally

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,8 @@
+class CouponsController < ApplicationController
+   
+    def index
+        @coupons = Coupon.all
+    end
+
+end
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+coupon1 = Coupon.create(coupon_code: 'E12S', store: 'chipotle')
+coupon2 = Coupon.create(coupon_code: 'E15S', store: 'panda express')
